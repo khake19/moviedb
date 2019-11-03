58c268998fcc8467f7a6d4245fc79068ac15c0b8
khake19 <ed@acudeen.com>
2019-11-03 13:08:55 +0800

    Add custom button and form input field

---
+++ components/FormTextInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, TextInput, TextInputProps } from 'react-native';
+import colors from '../config/colors';
+
+const FormTextInput = (props) => {
+
+  const { style, ...otherProps } = props;
+  return (
+    <TextInput
+      selectionColor={colors.DODGER_BLUE}
+      style={[styles.textInput, style]}
+      {...otherProps}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    borderColor: colors.SILVER,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginBottom: 20
+  }
+})
+
+export default FormTextInput;
